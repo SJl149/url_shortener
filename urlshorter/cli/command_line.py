5ccf82b83601ca4616632ad98cc6b995e91ccea4
ScottL <sjlteacher@gmail.com>
2022-05-03 14:51:02 -0400

    CLI interface added to interact with app

---
+++ urlshorter/cli/command_line.py
@@ -0,0 +1,70 @@
+from urlshorter.datalayer.mocked_datalayer import MockedDatalayer
+from urlshorter.shortener.urlshorter_engine import URLShorterEngine
+import helpers
+
+def add_url(engine: URLShorterEngine) -> None:
+    long_url, custom_key = helpers.enter_new_url()
+    if custom_key:
+        added_key = engine.add_with_custom_key(custom_key, long_url)
+    else:
+        added_key = engine.add_with_random_key(long_url)
+
+    if added_key:
+        print(f"\n{long_url} can be accessed with urlshorter/{added_key}")
+    else:
+        print(f"\n    Error trying to add {long_url}!!! Please try again")
+
+
+def find_long_url(engine: URLShorterEngine) -> None:
+    key, long_url = None, None
+    while key is None and long_url is None:
+        key = helpers.enter_short_url_key()
+        long_url = engine.get_long_url(key)
+    if long_url:
+        print(f"The short URL urlshorter/{key} corresponds to {long_url}")
+    else:
+        print(f"Could not find URL with key: {key}")
+
+def get_stats(engine: URLShorterEngine) -> None:
+    key = helpers.enter_short_url_key()
+    response = engine.get_clicked_stats(key)
+    if response:
+        print(f"\nurlshorter/{key} has been clicked {response} times")
+    else:
+        print(f"     Could not find short url: urlshorter/{key}")
+
+def delete_url(engine: URLShorterEngine):
+    key = helpers.enter_short_url_key()
+    response = engine.delete_url(key)
+    if response:
+        print(f"urlshorter/{key} was deleted successfully!")
+    else:
+        print(f"There was a problem deleting urlshorter/{key}")
+    
+
+def menu_actions(action: str, engine: URLShorterEngine):
+    if action == "1":
+        add_url(engine)
+    elif action == "2":
+        find_long_url(engine)
+    elif action == "3":
+        get_stats(engine)
+    elif  action == "4":
+        delete_url(engine)
+    else:
+        helpers.quit_program()
+    
+if __name__ == "__main__":
+    helpers.greeting()
+    cont = True
+    datalayer = MockedDatalayer()
+    engine = URLShorterEngine(datalayer)
+
+    while cont:
+        helpers.print_menu()
+        action = helpers.get_menu_choice()
+        menu_actions(action, engine)
+        cont = helpers.ask_to_continue()
+    
+    helpers.quit_program()
+            
---
+++ urlshorter/cli/helpers.py
@@ -0,0 +1,51 @@
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def greeting():
+    clear_screen()
+    print("###  Welcome to URLShorter!  ###\n")
+
+def quit_program():
+    print("\nThank you for using URLShorter!\n\n")
+    exit()
+
+
+def print_menu():
+    print("\nMenu:")
+    print("    1. Add URL")
+    print("    2. Find long URL")
+    print("    3. Get stats on number of times a short URL was clicked")
+    print("    4. Delete URL")
+    print("    5. Quit\n")
+
+def get_menu_choice():
+    choice = input("Select option: ")
+    while not choice.isdigit() or int(choice) < 0 or int(choice) > 5:
+        print("\nInvalid choice: please enter a number from 1-5!\n")
+        choice = input("     Please enter a number from the above menu: ")
+    return choice
+
+def ask_to_continue():
+    response = input("\nPress <return> to continue or enter <quit> to exit: ")
+    if response in ["quit", "q", "Q"]:
+        return False
+    else:
+        return True
+
+def enter_new_url():
+    long_url = input("\nEnter long URL to add: ")
+    while len(long_url) < 1:
+        long_url = input("\n     Please enter a URL with length greater than 1: ")
+    custom_key = input("\nEnter a custom key or press <return> to randomly generate key: ")
+    while custom_key != "" and len(custom_key) > 6:
+        print("\n     Custom keys must be strings of 6 alphanumeric characters (ex: abc123)")
+        custom_key = input("\nEnter a custom key or press <return> to randomly generate key: ")
+    return long_url, custom_key
+
+def enter_short_url_key():
+    key = input("\nEnter shortened URL key (ex: abc123 from urlshorter/abc123) to find original: ")
+    while len(key) != 6:
+        key = input("\n    Invalid key. Please enter the last 6 characters from the shortened URL: ")
+    return key
